ad67c1f74496dd4f80b2ab99498f2d07952f879f
gsus <jlopeza@grupohi.mx>
2021-04-24 02:50:08 -0500

    Ajustes en estilos y tamaño de menu

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 require("partials/header.php");
 ?>
 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio debitis rem aut eligendi assumenda sint ea cumque, laboriosam repellat</p>
+
 <?php
 require("partials/footer.php");
 ?>
