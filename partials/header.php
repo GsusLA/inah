01d7645d6ad88394819526d43b9288d7c71edc1b
gsus <jlopeza@grupohi.mx>
2021-04-24 02:01:20 -0500

    Plantilla base con header, menu responsivo y footer

---
+++ partials/header.php
@@ -0,0 +1,52 @@
+<?php
+require("partials/main.php");
+?>
+<div class="container">
+    <div class="row" style="background-color: #4B0082;">
+        <div class="col-md-12 image-container">
+            <img src="assets/img/snpicd.png" alt="">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <p class="gestion">Gestión 2019 - 2022
+            </p>
+        </div>
+    </div>
+    
+    <div class="row">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-custom">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mx-auto">
+                    <li class="nav-item"><a class="nav-link" href="index">Inicio</a></li>
+                    <li class="nav-item"><a class="nav-link" href="somos">Quienes Somos</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Commité Ejecutivo</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Comunicación y Prensa</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Normateca</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">Ubicación</a></li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Transparecia
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">Unidad de Transparencia<br> (Ubicación)</a>
+                            <a class="dropdown-item" href="#">Comité de Transparencia</a>
+                            <a class="dropdown-item" href="https://consultapublicamx.inai.org.mx/vut-web/?idSujetoObigadoParametro=20299&idEntidadParametro=33&idSectorParametro=20" target="_blank"><img src="assets/img/01.jpg" alt="" class="img-fluid" style="width:170px;height:60px; margin-top: 5px; "></a>
+                            <a class="dropdown-item" href="#">Normatividad Aplicable</a>
+                            <a class="dropdown-item" href="#">Expedientes Reservados </a>
+                            <a class="dropdown-item" href="#">Días Inhábiles</a>
+                            <a class="dropdown-item" href="#">Denuncias</a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+
+    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+    <script src="assets/js/header.js"></script>
+</div>
