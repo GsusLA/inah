5b6b72262ef463f95aca4ab4c71c2f332461b4ff
gsus <jlopeza@grupohi.mx>
2021-04-24 02:00:08 -0500

    Palntilla base con header, menu responsivo y footer

--- index.php
+++ index.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inah</title>
-</head>
-<body>
-    <p>Lorem ipsum dolor sit amet</p>
-    <a href="somos">somos</a>
-</body>
-</html>
+<?php
+require("partials/header.php");
+?>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio debitis rem aut eligendi assumenda sint ea cumque, laboriosam repellat</p>
+<?php
+require("partials/footer.php");
+?>
--- somos.php
+++ somos.php
@@ -1,11 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Somos</title>
-</head>
-<body>
-    <p>panditas</p>
-</body>
-</html>
+<?php
+require("partials/header.php");
+require("partials/footer.php");
+?>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis excepturi odit, animi, quae atque autem quas consectetur molestias nesciunt, illum praesentium enim. Eos dolorum, enim maxime nobis saepe quo aut.</p>
+<?php
+require("partials/footer.php");
+?>
