0fbb6216ec81fb11e86630bba2e02fa03f84e647
gsus <jlopeza@grupohi.mx>
2021-04-23 21:21:26 -0500

    Primer commit

---
+++ views/main.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inah</title>
+</head>
+<body>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus vero quis optio magni voluptatum numquam animi provident ex eum, autem tempora porro, a alias nostrum. Provident qui quae facere quia!</p>
+    <a href="somos">somos</a>
+</body>
+</html>
