76f34f811d2038cd5109841b5361b05a1ef69ddf
Gsus <gsus.arriaga@gmail.com>
2023-05-10 15:47:21 -0500

    Actualizacion en avisos de denuncias

--- gestion2023/denuncias.php
+++ gestion2023/denuncias.php
@@ -61,7 +61,7 @@ require("partials/header.php");
     <div class="row justify-content-center">
         <div class="col-lg-10 col-md-12 col-12 ">
             <p style="text-align: justify; text-justify: inter-word;"><img src="assets/icons/quote_left.png" alt="Ver" class="ico-pdf"> 
-            “En el periodo comprendido del 1 de julio de 2022 al 30 de septiembre de 2022, el Sindicato Nacional de Profesores de Investigación Científica y Docencia del INAH, no cuenta con denuncias ciudadanas ya sea por incumplimiento o por falta de actualización de las Obligaciones de Transparencia”
+            “Durante el mes de julio de 2022, el Sindicato Nacional de Profesores de Investigación Científica y Docencia del INAH, no cuenta con denuncias ciudadanas ya sea por incumplimiento o por falta de actualización de las Obligaciones de Transparencia”
                 <img src="assets/icons/quote_right.png" alt="Ver" class="ico-pdf">
             </p>
             <hr>
@@ -70,7 +70,79 @@ require("partials/header.php");
     <div class="row justify-content-center">
         <div class="col-lg-10 col-md-12 col-12 ">
             <p style="text-align: justify; text-justify: inter-word;"><img src="assets/icons/quote_left.png" alt="Ver" class="ico-pdf"> 
-            “En el periodo comprendido del 1 de octubre de 2022 al 31 de diciembre de 2022, el Sindicato Nacional de Profesores de Investigación Científica y Docencia del INAH, no cuenta con denuncias ciudadanas ya sea por incumplimiento o por falta de actualización de las Obligaciones de Transparencia”
+            “Durante el mes de agosto de 2022, el Sindicato Nacional de Profesores de Investigación Científica y Docencia del INAH, no cuenta con denuncias ciudadanas ya sea por incumplimiento o por falta de actualización de las Obligaciones de Transparencia”
+                <img src="assets/icons/quote_right.png" alt="Ver" class="ico-pdf">
+            </p>
+            <hr>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-lg-10 col-md-12 col-12 ">
+            <p style="text-align: justify; text-justify: inter-word;"><img src="assets/icons/quote_left.png" alt="Ver" class="ico-pdf"> 
+            “Durante el mes de septiembre de 2022, el Sindicato Nacional de Profesores de Investigación Científica y Docencia del INAH, no cuenta con denuncias ciudadanas ya sea por incumplimiento o por falta de actualización de las Obligaciones de Transparencia”
+                <img src="assets/icons/quote_right.png" alt="Ver" class="ico-pdf">
+            </p>
+            <hr>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-lg-10 col-md-12 col-12 ">
+            <p style="text-align: justify; text-justify: inter-word;"><img src="assets/icons/quote_left.png" alt="Ver" class="ico-pdf"> 
+            “Durante el mes de octubre de 2022, el Sindicato Nacional de Profesores de Investigación Científica y Docencia del INAH, no cuenta con denuncias ciudadanas ya sea por incumplimiento o por falta de actualización de las Obligaciones de Transparencia”
+                <img src="assets/icons/quote_right.png" alt="Ver" class="ico-pdf">
+            </p>
+            <hr>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-lg-10 col-md-12 col-12 ">
+            <p style="text-align: justify; text-justify: inter-word;"><img src="assets/icons/quote_left.png" alt="Ver" class="ico-pdf"> 
+            “Durante el mes de noviembre de 2022, el Sindicato Nacional de Profesores de Investigación Científica y Docencia del INAH, no cuenta con denuncias ciudadanas ya sea por incumplimiento o por falta de actualización de las Obligaciones de Transparencia”
+                <img src="assets/icons/quote_right.png" alt="Ver" class="ico-pdf">
+            </p>
+            <hr>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-lg-10 col-md-12 col-12 ">
+            <p style="text-align: justify; text-justify: inter-word;"><img src="assets/icons/quote_left.png" alt="Ver" class="ico-pdf"> 
+            “Durante el mes de diciembre de 2022, el Sindicato Nacional de Profesores de Investigación Científica y Docencia del INAH, no cuenta con denuncias ciudadanas ya sea por incumplimiento o por falta de actualización de las Obligaciones de Transparencia”
+                <img src="assets/icons/quote_right.png" alt="Ver" class="ico-pdf">
+            </p>
+            <hr>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-lg-10 col-md-12 col-12 ">
+            <p style="text-align: justify; text-justify: inter-word;"><img src="assets/icons/quote_left.png" alt="Ver" class="ico-pdf"> 
+            “Durante el mes de enero de 2023, el Sindicato Nacional de Profesores de Investigación Científica y Docencia del INAH, no cuenta con denuncias ciudadanas ya sea por incumplimiento o por falta de actualización de las Obligaciones de Transparencia”
+                <img src="assets/icons/quote_right.png" alt="Ver" class="ico-pdf">
+            </p>
+            <hr>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-lg-10 col-md-12 col-12 ">
+            <p style="text-align: justify; text-justify: inter-word;"><img src="assets/icons/quote_left.png" alt="Ver" class="ico-pdf"> 
+            “Durante el mes de febrero de 2023, el Sindicato Nacional de Profesores de Investigación Científica y Docencia del INAH, no cuenta con denuncias ciudadanas ya sea por incumplimiento o por falta de actualización de las Obligaciones de Transparencia”
+                <img src="assets/icons/quote_right.png" alt="Ver" class="ico-pdf">
+            </p>
+            <hr>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-lg-10 col-md-12 col-12 ">
+            <p style="text-align: justify; text-justify: inter-word;"><img src="assets/icons/quote_left.png" alt="Ver" class="ico-pdf"> 
+            “Durante el mes de marzo de 2023, el Sindicato Nacional de Profesores de Investigación Científica y Docencia del INAH, no cuenta con denuncias ciudadanas ya sea por incumplimiento o por falta de actualización de las Obligaciones de Transparencia”
+                <img src="assets/icons/quote_right.png" alt="Ver" class="ico-pdf">
+            </p>
+            <hr>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-lg-10 col-md-12 col-12 ">
+            <p style="text-align: justify; text-justify: inter-word;"><img src="assets/icons/quote_left.png" alt="Ver" class="ico-pdf"> 
+            “Durante el mes de abril de 2023, el Sindicato Nacional de Profesores de Investigación Científica y Docencia del INAH, no cuenta con denuncias ciudadanas ya sea por incumplimiento o por falta de actualización de las Obligaciones de Transparencia”
                 <img src="assets/icons/quote_right.png" alt="Ver" class="ico-pdf">
             </p>
             <hr>
